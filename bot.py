290d2553911478d6f5f9643726ff8884d0b827e9
YuKitAs <moncher.merci@gmail.com>
2020-03-21 17:37:24 +0100

    Adjust text

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 
 from telegram.ext import Updater, Filters, MessageHandler
 
@@ -9,14 +10,33 @@ import util
 util.setup_logging()
 logger = logging.getLogger(__name__)
 
+welcome_text = "Hallo {}, " \
+               "schoen, dass Du hier bist.\n" \
+               "Wir wollen Menschen in Not mit Aerzten oder Psychologen via Chat-bot zusammen bringen. Eine Art bedarfsorientiertes Corona-Chat-Roulette.\n" \
+               "- Ein Patient in Indien kann vielleicht im Moment keinen Arzt bei sich kontaktieren und nutzt unseren Bot, um mit einem Arzt in Italien zu sprechen\n" \
+               "- Ein Arzt, der psychisch ueberfordert ist, muss vielleicht kurz mit einem Psychologen sprechen\n" \
+               "Im ersten Schritt, wollen wir einfach einen Bot bauen, der anhand von zwei Fragen, die Person entweder als \"medizinischen Bedarf\" oder als \"psychologischen Bedarf\" einstuft.\n" \
+               "Eine kurze Beschreibung findest Du hier https://humanbios.org/#/\n" \
+               "Eine Spec findest Du hier https://hackmd.io/p0vKHdtAR4C1ygXadeTncA?view\n" \
+               "Sprich gerne @TobiasHeldt an, wenn Du mitmachen willst.\n\n" \
+               "Hi {}, " \
+               "nice that you're here.\n" \
+               "We want to bring people in need together with doctors or psychologists via a telegram chat bot. Some kind of need-based corona chat roulette.\n" \
+               "- A patient in India may be unable to contact a doctor at the moment and is using our bot to speak to a doctor in Italy\n" \
+               "- A doctor who is mentally overwhelmed may need to speak to a psychologist briefly\n" \
+               "In the first step, we simply want to build a bot that, based on two questions, classifies the person as either \"medical need\" or \"psychological need\".\n" \
+               "You can find a short description here https://humanbios.org/#/\n" \
+               "You can find a spec here https://hackmd.io/p0vKHdtAR4C1ygXadeTncA?view\n" \
+               "Talk to @TobiasHeldt if you want to join.\n"
 
-def greet(update, context):
+
+def greet(update, _context):
     """Sends a greeting message to the user who joined!"""
-    welcome_text = config.GREET_TEXT
-    update.message.reply_text(welcome_text.format(update.effective_user.first_name))
+    new_member_name = update.message.new_chat_members[0].name
+    update.message.reply_text(welcome_text.format(new_member_name, new_member_name))
 
 
-updater = Updater(token=config.BOT_TOKEN, use_context=True)
+updater = Updater(token=os.getenv("TOKEN"), use_context=True)
 
 greeting_handler = MessageHandler(Filters.group & Filters.status_update.new_chat_members, greet)
 updater.dispatcher.add_handler(greeting_handler)
