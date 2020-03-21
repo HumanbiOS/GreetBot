08f6857a389fc4a3fa3d99cedabe8c13b0718314
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-21 14:43:11 +0100

    feat(config): add sample config file

---
+++ config.sample.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+
+# Webhook configuration - If set to false we use long polling
+USE_WEBHOOK = False
+WEBHOOK_PORT = 9001
+WEBHOOK_URL = "https://domain.example.com/" + BOT_TOKEN
+CERTPATH = "/etc/certs/example.com/fullchain.cer"
