e7327453e7c6be2a82791940a8ea9f72cbb5d9e3
YuKitAs <moncher.merci@gmail.com>
2020-03-29 14:54:39 +0200

    Send text according to language code

--- bot.py
+++ bot.py
@@ -5,35 +5,20 @@ import os
 from telegram.ext import Updater, Filters, MessageHandler
 
 import config
+import text
 import util
 
 util.setup_logging()
 logger = logging.getLogger(__name__)
 
-welcome_text = "Hallo {}, " \
-               "schoen, dass Du hier bist.\n" \
-               "Wir wollen Menschen in Not mit Aerzten oder Psychologen via Chat-bot zusammen bringen. Eine Art bedarfsorientiertes Corona-Chat-Roulette.\n" \
-               "- Ein Patient in Indien kann vielleicht im Moment keinen Arzt bei sich kontaktieren und nutzt unseren Bot, um mit einem Arzt in Italien zu sprechen\n" \
-               "- Ein Arzt, der psychisch ueberfordert ist, muss vielleicht kurz mit einem Psychologen sprechen\n" \
-               "Im ersten Schritt, wollen wir einfach einen Bot bauen, der anhand von zwei Fragen, die Person entweder als \"medizinischen Bedarf\" oder als \"psychologischen Bedarf\" einstuft.\n" \
-               "- Wer wir sind https://humanbios.org/#/\n" \
-               "- GitHub https://github.com/HumanbiOS\n" \
-               "Sprich gerne @TobiasHeldt an, wenn Du mitmachen willst.\n\n" \
-               "Hi {}, " \
-               "nice that you're here.\n" \
-               "We want to bring people in need together with doctors or psychologists via a telegram chat bot. Some kind of need-based corona chat roulette.\n" \
-               "- A patient in India may be unable to contact a doctor at the moment and is using our bot to speak to a doctor in Italy\n" \
-               "- A doctor who is mentally overwhelmed may need to speak to a psychologist briefly\n" \
-               "In the first step, we simply want to build a bot that, based on two questions, classifies the person as either \"medical need\" or \"psychological need\".\n" \
-               "- Who we are https://humanbios.org/#/\n" \
-               "- GitHub https://github.com/HumanbiOS\n" \
-               "Talk to @TobiasHeldt if you want to join.\n"
-
 
 def greet(update, _context):
     """Sends a greeting message to the user who joined!"""
-    new_member_name = update.message.new_chat_members[0].name
-    update.message.reply_text(welcome_text.format(new_member_name, new_member_name))
+    new_member = update.message.new_chat_members[0]
+    if new_member.language_code == 'de':
+        update.message.reply_text(text.WELCOME_TEXT_DE.format(new_member.name))
+    else:
+        update.message.reply_text(text.WELCOME_TEXT_EN.format(new_member.name))
 
 
 TOKEN = os.getenv("TOKEN")
---
+++ text.py
@@ -0,0 +1,19 @@
+WELCOME_TEXT_DE = "Hallo {}, " \
+                  "schoen, dass Du hier bist.\n" \
+                  "Wir wollen Menschen in Not mit Aerzten oder Psychologen via Chat-bot zusammen bringen. Eine Art bedarfsorientiertes Corona-Chat-Roulette.\n" \
+                  "- Ein Patient in Indien kann vielleicht im Moment keinen Arzt bei sich kontaktieren und nutzt unseren Bot, um mit einem Arzt in Italien zu sprechen\n" \
+                  "- Ein Arzt, der psychisch ueberfordert ist, muss vielleicht kurz mit einem Psychologen sprechen\n" \
+                  "Im ersten Schritt, wollen wir einfach einen Bot bauen, der anhand von zwei Fragen, die Person entweder als \"medizinischen Bedarf\" oder als \"psychologischen Bedarf\" einstuft.\n" \
+                  "- Wer wir sind https://humanbios.org/#/\n" \
+                  "- GitHub https://github.com/HumanbiOS\n" \
+                  "Sprich gerne @TobiasHeldt an, wenn Du mitmachen willst.\n"
+
+WELCOME_TEXT_EN = "Hi {}, " \
+                  "nice that you're here.\n" \
+                  "We want to bring people in need together with doctors or psychologists via a telegram chat bot. Some kind of need-based corona chat roulette.\n" \
+                  "- A patient in India may be unable to contact a doctor at the moment and is using our bot to speak to a doctor in Italy\n" \
+                  "- A doctor who is mentally overwhelmed may need to speak to a psychologist briefly\n" \
+                  "In the first step, we simply want to build a bot that, based on two questions, classifies the person as either \"medical need\" or \"psychological need\".\n" \
+                  "- Who we are https://humanbios.org/#/\n" \
+                  "- GitHub https://github.com/HumanbiOS\n" \
+                  "Talk to @TobiasHeldt if you want to join.\n"
